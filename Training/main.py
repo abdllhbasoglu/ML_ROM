9677c68ce0460864ddcdd8921ddb63406a297b09
Abdullah Basoglu <155046523+abdllhbasoglu@users.noreply.github.com>
2024-05-22 20:27:21 +0900

    Update main.py

--- Training/main.py
+++ Training/main.py
@@ -18,7 +18,7 @@
 #######################################################################################
 
 
-from Model_B15 import Autoencoder
+from Model_B16 import Autoencoder
 
 import vessl
 import numpy as np
